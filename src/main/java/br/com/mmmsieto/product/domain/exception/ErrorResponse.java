ec27b14d2e6ac9326bdbf2ebff97f319e890b045
devmarciosieto <mmmsieto@hotmail.com>
2024-03-14 22:23:52 -0300

    add ApiExceptionHandler

--- src/main/java/br/com/mmmsieto/product/domain/exception/ErrorResponse.java
+++ src/main/java/br/com/mmmsieto/product/domain/exception/ErrorResponse.java
@@ -1,12 +1,12 @@
 package br.com.mmmsieto.product.domain.exception;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
 
     private Long timestamp;
