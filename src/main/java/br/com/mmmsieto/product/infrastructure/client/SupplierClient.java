367f173974b35b79f13d97e0be8c4eaff2f2d3d3
devmarciosieto <mmmsieto@hotmail.com>
2024-03-15 23:05:58 -0300

    add feingClient

---
+++ src/main/java/br/com/mmmsieto/product/infrastructure/client/SupplierClient.java
@@ -0,0 +1,16 @@
+package br.com.mmmsieto.product.infrastructure.client;
+
+import br.com.mmmsieto.product.infrastructure.entity.SupplierEntity;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "SupplierClient", url = "${client.supplier.url}")
+public interface SupplierClient {
+
+    @GetMapping("/suppliers/{idProduct}")
+    List<SupplierEntity> getSuppliers(@PathVariable("idProduct") String idProduct);
+
+}
---
+++ src/main/java/br/com/mmmsieto/product/infrastructure/entity/SupplierEntity.java
@@ -0,0 +1,13 @@
+package br.com.mmmsieto.product.infrastructure.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SupplierEntity {
+
+    private String name;
+    private String company;
+
+}
