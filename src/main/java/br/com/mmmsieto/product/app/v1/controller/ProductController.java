bc79dfc3b82350ddcae573b504734aa2a6498c7d
devmarciosieto <mmmsieto@hotmail.com>
2024-03-16 16:49:57 -0300

    add CircuitBreaker

--- src/main/java/br/com/mmmsieto/product/app/v1/controller/ProductController.java
+++ src/main/java/br/com/mmmsieto/product/app/v1/controller/ProductController.java
@@ -3,7 +3,6 @@ package br.com.mmmsieto.product.app.v1.controller;
 import br.com.mmmsieto.product.app.v1.controller.request.ProductRequest;
 import br.com.mmmsieto.product.app.v1.controller.response.ProductResponse;
 import br.com.mmmsieto.product.domain.service.ProductService;
-import br.com.mmmsieto.product.infrastructure.client.SupplierClient;
 import br.com.mmmsieto.product.infrastructure.mapper.ProductMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -17,14 +16,11 @@ public class ProductController {
 
     private final ProductService productService;
     private final ProductMapper productMapper;
-    private final SupplierClient supplierClient;
 
     public ProductController(ProductService productService,
-                             ProductMapper productMapper, SupplierClient supplierClient) {
+                             ProductMapper productMapper) {
         this.productService = productService;
         this.productMapper = productMapper;
-
-        this.supplierClient = supplierClient;
     }
 
     @PostMapping
@@ -36,12 +32,7 @@ public class ProductController {
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ProductResponse findById(@PathVariable("id") String id) {
-
-        String company = supplierClient.getSuppliers(id)
-                .stream().findFirst().orElse(null)
-                .getCompany();
-
-        return productMapper.toProductResponseWithCompany(productService.findById(id), company);
+        return productService.findById(id);
     }
 
     @GetMapping
--- src/main/java/br/com/mmmsieto/product/domain/service/ProductService.java
+++ src/main/java/br/com/mmmsieto/product/domain/service/ProductService.java
@@ -1,8 +1,11 @@
 package br.com.mmmsieto.product.domain.service;
 
+import br.com.mmmsieto.product.app.v1.controller.response.ProductResponse;
 import br.com.mmmsieto.product.domain.entity.ProductEntity;
+import br.com.mmmsieto.product.infrastructure.client.SupplierClient;
 import br.com.mmmsieto.product.infrastructure.mapper.ProductMapper;
 import br.com.mmmsieto.product.infrastructure.repository.ProductRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +16,33 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final SupplierClient supplierClient;
 
     public ProductService(ProductRepository productRepository,
-                          ProductMapper productMapper) {
+                          ProductMapper productMapper, SupplierClient supplierClient) {
         this.productRepository = productRepository;
         this.productMapper = productMapper;
+        this.supplierClient = supplierClient;
     }
 
     public void save(ProductEntity productEntity) {
         productRepository.save(productMapper.toProductModel(productEntity));
     }
 
-    public ProductEntity findById(String id) {
+    @CircuitBreaker(name = "suppliers")
+    public ProductResponse findById(String id) {
+
+        String company = supplierClient.getSuppliers(id)
+                .stream().findFirst().orElse(null)
+                .getCompany();
+
         ObjectId objectId = new ObjectId(id);
-        return productRepository.findById(objectId)
+
+        ProductEntity productEntity = productRepository.findById(objectId)
                 .map(productMapper::toProductEntity)
                 .orElseThrow(RuntimeException::new);
+
+        return productMapper.toProductResponseWithCompany(productEntity, company);
     }
 
     public List<ProductEntity> findAll() {
